a23843ee648b4924676fae7b42cf963507899094
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2025-07-31 15:07:02 +0800

    initAddProjectBtn
    - added event listener for add project button and
    created html form for inputting project title
    - changing form display to block and add project
    button to none when clicked to toggle visibility

---
+++ src/modules/website.js
@@ -0,0 +1,64 @@
+export default class Website {
+  static initAddProjectBtn() {
+    const addProjectBtn = document.querySelector('.add-project-btn');
+    const projectFormDiv = document.getElementById('project-form');
+    const projectDetailsForm = document.getElementById('project-details-form');
+    const cancelProjectBtn = document.getElementById('cancel-project-btn');
+    const projectNameInput = document.getElementById('project-name');
+
+    addProjectBtn.addEventListener('click', () => {
+      console.log('test');
+      projectFormDiv.style.display = 'block';
+      addProjectBtn.style.display = 'none';
+    })
+
+    projectDetailsForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const projectName = projectNameInput.value;
+      if (projectName) {
+        this.createProject(projectName);
+        projectFormDiv.style.display = 'none';
+        addProjectBtn.style.display = 'block';
+        projectNameInput.value = '';
+      }
+      else {
+        alert('Please enter a project name');
+      }
+    })
+
+    cancelProjectBtn.addEventListener('click', () => {
+      projectNameInput.value = '';
+      projectFormDiv.style.display = 'none';
+      addProjectBtn.style.display = 'block';
+    })
+  }
+  // createProject(name) {
+  //   const projectsContainer = document.querySelector('.projects-container');
+  //   const newProject = document.createElement('div');
+  //   newProject.classList.add(`project ${name}`);
+  //   const newProjectBtn = document.createElement('button');
+  //   newProjectBtn.classList.add(`project-btn ${name}`);
+  //   newProjectBtn.textContent = name;
+  //   const deleteProjectBtn = document.createElement('button');
+  //   deleteProjectBtn.textContent = 'X';
+  //   deleteProjectBtn.classList.add('delete-project-btn');
+  //   newProject.appendChild(newProjectBtn);
+  //   newProject.appendChild(deleteProjectBtn);
+  //   projectsContainer.appendChild(newProject);
+  // }
+
+  // loadProject(title) {
+  //   const todosContainer = document.querySelector('.todos-container');
+  //   todosContainer.innerHTML = '';
+  //   const newTodo = document.createElement('div');
+  // }
+
+  // initiateProjectBtns() {
+  //   const projectBtns = document.querySelectorAll('.project-btn');
+  //   const deleteProjectBtns = document.querySelectorAll('.delete-project-btn');
+
+  //   projectBtns.forEach(btn => {
+  //     btn.addEventListener('click', Website.loadProject(btn.textContent))
+  //   })
+  // }
+}
